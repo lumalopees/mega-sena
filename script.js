889aa1b68e4de30f3db1e1f59915c46efbb50d79
Luma <luuh_lopees@live.com>
2023-09-20 14:54:53 -0300

    ✨ 25min | header pronta

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// criar elemento para o header 
-// onde eu quero criar ele? no header
+// funções
 
-const getHeader = document.getElementsByTagName('header')[0];
-const createTitle = document.createElement('h1')
-createTitle.innerText = 'Boas vindas - bora jogar na mega?'
-const createSubtext = document.createElement('h3')
-createSubtext.innerText = 'Números Sorteados'
+const createHeader = () => {
+  const getHeader = document.getElementsByTagName('header')[0];
+  const createTitle = document.createElement('h1')
+  createTitle.innerText = 'Boas vindas - bora jogar na mega?'
+  const createSubtext = document.createElement('h3')
+  createSubtext.innerText = 'Números Sorteados'
+  getHeader.appendChild(createTitle);
+  getHeader.appendChild(createSubtext);
+}
 
-getHeader.appendChild(createTitle);
-getHeader.appendChild(createSubtext);
+
+// chamada de funções
+
+createHeader();
